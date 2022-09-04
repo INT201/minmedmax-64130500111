19fd66710f8f209afcd17a519b480d1b068b6eb8
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-04 19:17:54 +0700

    v3 เมื่อกี้รีบไปหน่อย

--- minMedMax.js
+++ minMedMax.js
@@ -1,4 +1,4 @@
-// const { template } = require("@babel/core");
+const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
   let min 
@@ -63,11 +63,11 @@ function minMedMax(n1, n2, n3) {
    return Answer;
 }
 
-console.log(minMedMax(85, 30, 1));
-console.log(minMedMax(10, 0, 20));
-console.log(minMedMax(-5, 0, 10));
-console.log(minMedMax(5, 1, 1));
-console.log(minMedMax(-1, -8, 0));
+// console.log(minMedMax(85, 30, 1));
+// console.log(minMedMax(10, 0, 20));
+// console.log(minMedMax(-5, 0, 10));
+// console.log(minMedMax(5, 1, 1));
+// console.log(minMedMax(-1, -8, 0));
 
 
-// module.exports = minMedMax;
+module.exports = minMedMax;
